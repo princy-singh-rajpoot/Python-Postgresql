c3f52db211cdc983550b8ef1b3400cb4e4eab01a
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-08 01:47:11 +0530

    Ignore .pyc files

--- backend_api/main/models.py
+++ backend_api/main/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# class Vendor(models.model):
->>>>>>> 417a2fac13ce5b2a5b22a2825cc31974a06d9578
+class Vendor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField(null=True)
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=50)
+    detail = models.TextField(null=True)
+    
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    price = models.FloatField()
+    
+    def __str__(self):
+        return self.title
