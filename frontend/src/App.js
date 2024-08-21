c260c50a7f388da170e91ec454687f5898b4f793
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-21 18:07:55 +0530

    adding frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-warning">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
         <div className="container">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -13,10 +13,9 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <a className="navbar-brand" href="#">Python Market Place</a>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item"><a className="nav-link active" aria-current="page" href="#"> Django Market</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Link</a></li>
-              <li className="nav-item"><a className="nav-link disabled" aria-disabled="true">Disabled</a></li>
-            </ul>
+              <li className="nav-item"><a className="nav-link active" aria-current="page" href="#"> Home </a></li>
+              <li className="nav-item"><a className="nav-link" href="#">Categories</a></li>
+             </ul>
           </div>
         </div>
       </nav>
