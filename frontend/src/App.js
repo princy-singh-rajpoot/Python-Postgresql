fcaa119784bdb6db99238952a620b640f0a19ef4
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-23 14:20:13 +0530

    commiting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
         <div className='row'>
           <div className='col-12 col-md-3 mb-2'>
             <div className="card">
-              <img src={{logo}} className="card-img-top" alt="..."/>
+              <img src={logo} className="card-img-top" alt="..."/>
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
