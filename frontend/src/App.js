f5123df31dbf6d40fd503aa94a1b8151b1c92003
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-25 21:10:10 +0530

    commiting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -483,21 +483,21 @@ function App() {
 
           </div>
           {/* end of rating and reviews */}
+          <footer className="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
 
-          <footer class="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+          <div className="col-md-4 d-flex align-items-center">
+              <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">PythonScript</a>
+              <span className="mb-3 mb-md-0 text-muted">Company 2024</span>
+          </div>
 
-            <div class="col-md-4 d-flex align-items-center">
-               <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"> PythonScript </a>
-               <span class="mb-3 mb-md-0 text-muted"> Company 2024 </span>
-            </div>
-
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex pt-2">
-              <li class="ms-3"><a class="text-muted" href="#"><i className='fa-brands fa-facebook fa-2x'></i></a></li>
-              <li class="ms-3"><a class="text-muted" href="#"><i className='fa-brands fa-instagram fa-2x'></i></a></li>
-              <li class="ms-3"><a class="text-muted" href="#"><i className='fa-brands fa-twitter fa-2x'></i></a></li>
-            </ul>
+          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex pt-2">
+              <li className="ms-3"><a className="text-muted" href="#"><i className="fa-brands fa-facebook fa-2x"></i></a></li>
+              <li className="ms-3"><a className="text-muted" href="#"><i className="fa-brands fa-instagram fa-2x"></i></a></li>
+              <li className="ms-3"><a className="text-muted" href="#"><i className="fa-brands fa-twitter fa-2x"></i></a></li>
+          </ul>
 
           </footer>
+
 
         </div>
       </main>
