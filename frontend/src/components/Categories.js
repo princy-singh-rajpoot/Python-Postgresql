3493bc3745083fc846677f793bcc19f2fd454a63
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-25 23:41:54 +0530

    commiting updates on Categories and home.js

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -5,7 +5,7 @@ function Categories() {
     return (
        <section className="container mt-4">
         {/* start popularrr categoriesssssssss */}
-            <h3 className='mb-0'> All Categories </h3>
+            <h3 className='mb-4'> All Categories </h3>
             <div className='row mb-2'>
 
                 {/* categories box 1*/}
@@ -96,6 +96,20 @@ function Categories() {
 
                     {/* categories box 5*/}
                     <div className='col-12 col-md-3 mb-3'>
+                        <div className="card">
+                            <img src={logo} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                            </div>             
+                            <div className='card-footer'>
+                                Product Downloads : 2331
+                            </div>   
+                        </div>
+                    </div>
+                    {/* categories box ended 5*/}
+
+                     {/* categories box 5*/}
+                     <div className='col-12 col-md-3 mb-3'>
                         <div className="card">
                             <img src={logo} className="card-img-top" alt="..."/>
                             <div className="card-body">
