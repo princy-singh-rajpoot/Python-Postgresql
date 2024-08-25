8b3188df2cf295c36a7736016467d11799878c63
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-25 23:38:06 +0530

    commiting updates on Categories and home.js

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,6 +1,118 @@
+import logo from '../logo.svg';
+import {Link} from 'react-router-dom';
+
 function Categories() {
     return (
-       <h1> Category List </h1>
+       <section className="container mt-4">
+        {/* start popularrr categoriesssssssss */}
+            <h3 className='mb-0'> All Categories </h3>
+            <div className='row mb-2'>
+
+                {/* categories box 1*/}
+                <div className='col-12 col-md-3 mb-3'>
+                    <div className="card">
+                        <img src={logo} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                        </div>             
+                        <div className='card-footer'>
+                            Product Downloads : 2331
+                        </div>   
+                    </div>
+                </div>
+                {/* categories box ended 1*/}
+
+                {/* categories box 2*/}
+                <div className='col-12 col-md-3 mb-3'>
+                    <div className="card">
+                        <img src={logo} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                        </div>             
+                        <div className='card-footer'>
+                            Product Downloads : 2331
+                        </div>   
+                    </div>
+                </div>
+                {/* categories box ended 2*/}
+
+                {/* categories box 3*/}
+                <div className='col-12 col-md-3 mb-3'>
+                    <div className="card">
+                        <img src={logo} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                        </div>             
+                        <div className='card-footer'>
+                            Product Downloads : 2331
+                        </div>   
+                    </div>
+                </div>
+                {/* categories box ended 3*/}
+
+                {/* categories box 4*/}
+                <div className='col-12 col-md-3 mb-3'>
+                    <div className="card">
+                        <img src={logo} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                        </div>             
+                        <div className='card-footer'>
+                            Product Downloads : 2331
+                        </div>   
+                    </div>
+                </div>
+                {/* categories box ended 4*/}
+
+                <div className='row mb-2'>
+
+                    {/* categories box 5*/}
+                    <div className='col-12 col-md-3 mb-3'>
+                        <div className="card">
+                            <img src={logo} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h4 className="card-title"> <Link to="/"> Category title </Link></h4>
+                            </div>             
+                            <div className='card-footer'>
+                                Product Downloads : 2331
+                            </div>   
+                        </div>
+                    </div>
+                    {/* categories box ended 5*/}
+
+                    {/* categories box 6*/}
+                    <div className='col-12 col-md-3 mb-3'>
+                        <div className="card">
+                            <img src={logo} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                            </div>             
+                            <div className='card-footer'>
+                                Product Downloads : 2331
+                            </div>   
+                        </div>
+                    </div>
+                    {/* categories box ended 5*/}
+
+                    {/* categories box 5*/}
+                    <div className='col-12 col-md-3 mb-3'>
+                        <div className="card">
+                            <img src={logo} className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h4 className="card-title"><Link to="/"> Category title </Link></h4>
+                            </div>             
+                            <div className='card-footer'>
+                                Product Downloads : 2331
+                            </div>   
+                        </div>
+                    </div>
+                    {/* categories box ended 5*/}
+
+                </div>
+
+            </div>
+            {/* end of popularrr categoriesssssssss */}
+       </section>
     )
 }
 export default Categories;
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
 return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+    <footer className="d-flex flex-wrap justify-content-between container align-items-center my-4 border-top">
         <div className="col-md-4 d-flex align-items-center">
             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">PythonScript</a>
             <span className="mb-3 mb-md-0 text-muted">Company 2024</span>
