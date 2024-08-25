c0a068848fa615e51c508193ef5e1b96723a6985
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-25 23:15:52 +0530

    commiting updates on Categories and home.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,25 @@
+import {Link} from 'react-router-dom';
+
 function Header(){
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container">
-            <a className="navbar-brand" href="#">Python Market Place</a>
+            <a className="navbar-brand" href="#">Python Market Place </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item"><a className="nav-link" aria-current="page" href="#">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">Categories</a></li>
+                    <li className="nav-item">
+                        <Link className="nav-link" aria-current="page" to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/categories">
+                            Categories
+                        </Link>
+                    </li>
                 </ul>
             </div>
             </div>
