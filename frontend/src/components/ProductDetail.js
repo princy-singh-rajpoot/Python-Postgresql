38b1fca955e050b33f046d2179a8522cbc9c6ca0
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-26 22:38:39 +0530

    commiting category product

--- frontend/src/components/ProductDetail.js
+++ frontend/src/components/ProductDetail.js
@@ -17,7 +17,7 @@ function ProductDetail(){
                     <h5 className="card-title"> Price : Rs 500/- </h5>
                     <p className='mt-3'>
                         <button title="Add to cart" className="btn btn-success">
-                        <i className="fa-solid fa-cart-plus"></i>Add to cart 
+                        <i className="fa-solid fa-cart-plus"></i> Add to cart 
                         </button>
                         <button title="Buy now" className="btn btn-warning ms-1">
                         <i className="fa-solid fa-bag-shopping"></i> Buy Now
@@ -30,15 +30,15 @@ function ProductDetail(){
                     <div className='producttags mt-4'>
                         <h5> Tags </h5>
                         <p>
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link> ,  
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link> ,  
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link> ,
-                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link> ,  
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link>,  
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link>,  
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Python </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> Django </Link>,
+                            <Link to="#" className='badge bg-secondary text-white me-1'> JAVASCRIPT </Link>,  
                         </p>
                     </div>
                 </div>
