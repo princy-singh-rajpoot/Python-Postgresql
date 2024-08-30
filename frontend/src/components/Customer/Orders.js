215ead6371b348d7eb8531f7db67628d947e64d5
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-30 17:39:18 +0530

    commiting frontend

--- frontend/src/components/Customer/Orders.js
+++ frontend/src/components/Customer/Orders.js
@@ -1,6 +1,6 @@
 // Assets
 import logo from '../../logo.svg';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 function Orders() {
@@ -9,68 +9,87 @@ function Orders() {
             <div className="row">
 
                 <div className='col-md-3 col-12 mb-2'>
-                    <Sidebar/>
+                    <Sidebar />
                 </div>
 
                 <div className='col-md-9 col-12 mb-2'>
-
-                    <div className='row'>
-                        <div className='table-responsive'>
-                            <table className='table table-bordered'>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Product</th>
-                                        <th>Price</th>
-                                        <th>Action</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>
-                                        <Link to="#"><img src={logo} className="img-thumbnail" width='80' alt="..." /> DJango</Link>
-                                    </td>
-                                    <td>Rs. 500</td>
-                                    <td><span className='text-success'><i className='fa fa-check-circle'></i>Completed</span></td>
-                                    <td><button className='btn btn-primary btn-sm'>View</button></td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>
-                                        <Link to="#"><img src={logo} className="img-thumbnail" width='80' alt="..." /> Flask</Link>
-                                    </td>
-                                    <td>Rs. 500</td>
-                                    <td><span className='text-success'><i className='fa fa-check-circle'></i>Completed</span></td>
-                                    <td><button className='btn btn-primary btn-sm'>View</button></td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>
-                                        <Link to="#"><img src={logo} className="img-thumbnail" width='80' alt="..." /> Python</Link>
-                                    </td>
-                                    <td>Rs. 500</td>
-                                    <td><span className='text-secondary'><i className='fa fa-spin fa-spinner'></i>Processing</span></td>
-                                    {/* <td><button className='btn btn-primary btn-sm'>Downloads</button></td> */}
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>
-                                        <Link to="#"><img src={logo} className="img-thumbnail" width='80' alt="..." /> ReactJS</Link>
-                                    </td>
-                                    <td>Rs. 500</td>\<td><span className='text-danger'><i className='fa fa-times-circle'></i>Cancelled</span></td>
-                                    <td></td>
-                                </tr>
-                                </tbody>
-                            </table>
+                    <div className='card shadow-sm'>
+                        <div className='card-header bg-primary text-white'>
+                            <h5 className='mb-0'>Your Orders</h5>
+                        </div>
+                        <div className='card-body'>
+                            <div className='table-responsive'>
+                                <table className='table table-hover'>
+                                    <thead className='thead-dark'>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Product</th>
+                                            <th>Price</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>
+                                                <Link to="#" className='d-flex align-items-center'>
+                                                    <img src={logo} className="img-thumbnail me-2" width='60' alt="..." />
+                                                    <span>DJango</span>
+                                                </Link>
+                                            </td>
+                                            <td>Rs. 500</td>
+                                            <td><span className='badge bg-success'><i className='fa fa-check-circle'></i> Completed</span></td>
+                                            <td><button className='btn btn-primary btn-sm'><i className='fa fa-eye'></i> View</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>
+                                                <Link to="#" className='d-flex align-items-center'>
+                                                    <img src={logo} className="img-thumbnail me-2" width='60' alt="..." />
+                                                    <span>Flask</span>
+                                                </Link>
+                                            </td>
+                                            <td>Rs. 500</td>
+                                            <td><span className='badge bg-success'><i className='fa fa-check-circle'></i> Completed</span></td>
+                                            <td><button className='btn btn-primary btn-sm'><i className='fa fa-eye'></i> View</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>
+                                                <Link to="#" className='d-flex align-items-center'>
+                                                    <img src={logo} className="img-thumbnail me-2" width='60' alt="..." />
+                                                    <span>Python</span>
+                                                </Link>
+                                            </td>
+                                            <td>Rs. 500</td>
+                                            <td><span className='badge bg-warning text-dark'><i className='fa fa-spinner fa-spin'></i> Processing</span></td>
+                                            <td><button className='btn btn-secondary btn-sm' disabled><i className='fa fa-download'></i> Download</button></td>
+                                        </tr>
+                                        <tr>
+                                            <td>4</td>
+                                            <td>
+                                                <Link to="#" className='d-flex align-items-center'>
+                                                    <img src={logo} className="img-thumbnail me-2" width='60' alt="..." />
+                                                    <span>ReactJS</span>
+                                                </Link>
+                                            </td>
+                                            <td>Rs. 500</td>
+                                            <td><span className='badge bg-danger'><i className='fa fa-times-circle'></i> Cancelled</span></td>
+                                            <td><button className='btn btn-outline-danger btn-sm'><i className='fa fa-trash'></i> Remove</button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div className='card-footer text-center'>
+                            <Link to="#" className='btn btn-outline-primary btn-sm'>View All Orders</Link>
                         </div>
                     </div>
-
                 </div>
 
             </div>
         </div>
-      );
+    );
 }
 export default Orders;
